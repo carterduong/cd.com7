c5e697c4baa939fcb81c960bd65e68ed9db9fcbf
Carter Duong <info@carterduong.com>
2026-07-10 13:09:45 -0700

    Add appId

--- sanity/sanity.cli.ts
+++ sanity/sanity.cli.ts
@@ -3,9 +3,10 @@ import {defineCliConfig} from 'sanity/cli'
 export default defineCliConfig({
   api: {
     projectId: 'e3bdiegf',
-    dataset: 'production'
+    dataset: 'production',
   },
   deployment: {
+    appId: 'i1ty9k1g2xaleizfhg1mt657',
     /**
      * Enable auto-updates for studios.
      * Learn more at https://www.sanity.io/docs/studio/latest-version-of-sanity#k47faf43faf56
